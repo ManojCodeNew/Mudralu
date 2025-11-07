0e274e33b2647de62512f943846cd9670f46e89e
manoj <manojpoojary2004@gmail.com>
2025-11-07 15:45:15 +0530

    after updated vercel deploy error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,22 @@
 'use client'
-import { useState } from 'react'
-import { FiSearch } from 'react-icons/fi'
+// import { useState } from 'react'
+// import { FiSearch } from 'react-icons/fi'
 
 export default function Navbar () {
 
-  const [smallScreen, setSmallScreen] = useState(false)
-  const handleProfile = () => {}
+  // const [smallScreen, setSmallScreen] = useState(false)
   return (
     <nav className='flex justify-between items-center bg-[#FAF9F7] shadow-[0_4px_20px_rgba(0,0,0,0.05)] mb-3 px-4 py-3 rounded-b-3xl w-full text-gray-800'>
       {/* Left Logo */}
       <h1
-        className='font-bold text-orange-500 text-lg sm:text-xl md:text-2xl cursor-pointer'
+        className='px-3 py-2 font-bold text-orange-500 text-lg sm:text-xl md:text-2xl cursor-pointer'
         onClick={() => (window.location.href = '/')}
       >
         ಭಕ್ತಿ ಗೀತೆಗಳು
       </h1>
 
       {/* Search Bar */}
-      <div
+      {/* <div
         className={`${
           smallScreen
             ? 'max-h-20 mt-3 sm:mt-0'
@@ -31,12 +30,12 @@ export default function Navbar () {
         <input
           type='text'
           placeholder='ಹಾಡನ್ನು ಹುಡುಕಿ...'
-          className='flex-1 bg-transparent px-2 sm:px-3 py-2 rounded-full focus:outline-none text-sm sm:text-base text-gray-700 placeholder-gray-500'
+          className='flex-1 bg-transparent px-2 sm:px-3 py-2 rounded-full focus:outline-none text-gray-700 text-sm sm:text-base placeholder-gray-500'
         />
       </div>
-
+ */}
       {/* Right Icons */}
-      <div className='flex items-center gap-2'>
+      {/* <div className='flex items-center gap-2'>
         <FiSearch
           className='sm:hidden w-5 h-5 hover:text-orange-500 transition-colors cursor-pointer'
           onClick={() => {
@@ -46,15 +45,15 @@ export default function Navbar () {
               setSmallScreen(false)
             }
           }}
-        />
+        /> */}
 
-        <div
+        {/* <div
           className='flex justify-center bg-orange-100 p-1 border-2 border-gray-200 rounded-full w-8 h-8'
           onClick={handleProfile}
         >
           M
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </nav>
   )
 }
